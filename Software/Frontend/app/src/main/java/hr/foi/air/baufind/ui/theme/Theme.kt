ee3020b21b53e1e7083ba3c58f728929de8d04ae
Frano Šimić <fsimic21@gmail.com>
2024-11-14 15:23:57 +0100

    Popravljeno korisničko sučelje da odgovara skici zaslona

--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/theme/Theme.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/theme/Theme.kt
@@ -1,55 +1,44 @@
 package hr.foi.air.baufind.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFFF0000),
+    secondary = Color(0xFFFFEB3B),
+    tertiary = Color(0xFF2196F3),
+    background = Color(0xFF180C16),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color(0xFFFFFBFE),
+    onSecondary = Color(0xFFFFFBFE),
+    onTertiary = Color(0xFFFFFBFE),
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
+    primary = Color(0xFF2482E5),
+    secondary = Color(0xFF7EBDFF),
+    tertiary = Color(0xFF00519B),
+    background = Color(0xFFFAFAFA),
+    primaryContainer = Color(0xFFE8E8E8),
+    secondaryContainer = Color(0xFFFFFFFF),
+    onSecondary = Color(0xFF3F51B5),
+    onTertiary = Color(0xFF00BCD4),
     onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun BaufindTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
