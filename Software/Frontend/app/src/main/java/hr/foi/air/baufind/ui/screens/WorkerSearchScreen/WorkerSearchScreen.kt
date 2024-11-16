ef4e913b5dcb3743e6a97d4fa328614aa70080c4
mstura21-FOI <mustra769@gmail.com>
2024-11-16 23:48:44 +0100

    Dropdown meni napravljen, prema smjernicama treba koristiti chip umjesto dropdown menia

--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/WorkerSearchScreen/WorkerSearchScreen.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/WorkerSearchScreen/WorkerSearchScreen.kt
@@ -1,15 +1,13 @@
 package hr.foi.air.baufind.ui.screens.WorkerSearchScreen
 
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,18 +15,94 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WorkerSearchScreen(navController: NavController){
+fun WorkerSearchScreen(navController: NavController) {
     val viewModel: WorkerSearchViewModel = viewModel()
-    val scrollState = rememberScrollState()
+
+
+    val isExpandedL by viewModel.isExpandedL
+    val isExpandedR by viewModel.isExpandedR
+
+    val selectedItemL by viewModel.selectedItemL
+    val selectedItemR by viewModel.selectedItemR
+
+
+    val optionsR = listOf("Ocjena ASC", "Ocjena DESC", "Broj poslova ASC", "Broj poslova DESC")
+    val optionsL = listOf("Zagrebačka", "Krapinsko-zagorska", "Sisacko-moslavačka", "Karlovačka", "Varaždinska",
+         "Bjelovarsko-bilogorska", "Primorsko-goranska", "Ličko-senjska",
+        "Virovitičko-podravska", "Osječko-baranjska", "Šibensko-kninska", "Vukovarsko-srijemska",
+        "Zadarska", "Međimurska", "Dubrovničko-neretvanska", "Istarska", "Požeško-slavonska",
+        "Splitsko-dalmatinska", "Zagrebački grad"
+    )
+
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         Text("Worker Search Screen")
+
+        // Left Dropdown
+        Row(modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            ExposedDropdownMenuBox(
+                expanded = isExpandedL,
+                onExpandedChange = { viewModel.isExpandedL.value = it },
+                modifier = Modifier.fillMaxWidth(0.3f)
+            ) {
+                TextField(
+                    value = selectedItemL,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpandedL) },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(
+                    expanded = isExpandedL,
+                    onDismissRequest = { viewModel.isExpandedL.value = false }
+                ) {
+                    optionsL.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option) },
+                            onClick = {
+                                viewModel.selectedItemL.value = option
+                                viewModel.isExpandedL.value = false
+                            }
+                        )
+                    }
+                }
+            }
+            ExposedDropdownMenuBox(
+                expanded = isExpandedR,
+                onExpandedChange = { viewModel.isExpandedR.value = it },
+                modifier = Modifier.fillMaxWidth(0.4f)
+            ) {
+                TextField(
+                    value = selectedItemR,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpandedL) },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(
+                    expanded = isExpandedR,
+                    onDismissRequest = { viewModel.isExpandedL.value = false }
+                ) {
+                    optionsR.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option) },
+                            onClick = {
+                                viewModel.selectedItemR.value = option
+                                viewModel.isExpandedR.value = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+        
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun WorkerSearchScreenPreview(){
+fun WorkerSearchScreenPreview() {
     val navController = rememberNavController()
     WorkerSearchScreen(navController)
 }
-
--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/WorkerSearchScreen/WorkerSearchViewModel.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/WorkerSearchScreen/WorkerSearchViewModel.kt
@@ -1,8 +1,19 @@
 package hr.foi.air.baufind.ui.screens.WorkerSearchScreen
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import hr.foi.air.baufind.mock.WorkerSearchMock.WorkerMock
+
 class WorkerSearchViewModel : ViewModel() {
+
+    val isExpandedL: MutableState<Boolean> = mutableStateOf(false)
+    val isExpandedR: MutableState<Boolean> = mutableStateOf(false)
+
+
+    val selectedItemL: MutableState<String> = mutableStateOf("")
+    val selectedItemR: MutableState<String> = mutableStateOf("")
+
     val workers: MutableLiveData<List<WorkerMock.Worker>> = MutableLiveData(WorkerMock.workers)
 }
