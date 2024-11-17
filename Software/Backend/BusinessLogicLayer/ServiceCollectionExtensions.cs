6c2dea274d8683737cbe9515b948112f0f7cd421
dmatijani <dmatijani21@foi.hr>
2024-11-17 19:55:46 +0100

    BAUF-66 Kreirana klasa za povezivanje s bazom podataka
    Također je korišten ASP.NET-ov Options uzorak dizajna za dodavanje ConnectionString, koju je zato lako izmijeniti. Kreirana su i dva projekta, što predstavljaju slojeve, vidljive na sljedećem dokumentu pod "Backend": https://vlovric21.atlassian.net/wiki/spaces/baufind/pages/8847370/Arhitektura

---
+++ Software/Backend/BusinessLogicLayer/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogicLayer;
+
+/// <summary>
+/// Klasa za registriranje dodatnih servisa potrebnih za DataAccessLayer, a koji se ne mogu direktno ubaciti u WebApi projekt jer nema referencu na isti
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDataAccessLayerServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.Configure<DBOptions>(
+            configuration.GetSection("Database")
+        );
+
+        return services;
+    }
+}
