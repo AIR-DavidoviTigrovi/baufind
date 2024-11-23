75d7bafd3cda999c0d582ab6660337ab24c62160
Mateo Vujica <94994876+mvujica21@users.noreply.github.com>
2024-11-23 12:24:46 +0100

    Kreirao sam SkillModel i UserProfileModel za definiranje s kojim podacima ću raditi.

---
+++ Software/Backend/DataAccessLayer/Models/UserProfileModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DataAccessLayer.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public record UserProfileModel 
+{
+    public string Name {  get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public byte[]? ProfilePicture { get; set; }
+    public DateTime Joined { get; set; }
+
+    public List<SkillModel> Skills { get; set; }
+
+    public double WorkerRating { get; set; }
+    public double EmployerRating { get; set; }
+
+}
