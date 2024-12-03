d83c23b762e9f3785c8e29fee4a75a027d06b129
vlovric21 <vlovric21@student.foi.hr>
2024-12-03 17:46:32 +0100

    Napravljena validacija na backendu

--- Software/Backend/BusinessLogicLayer/AppLogic/Jobs/AddJob/AddJobValidator.cs
+++ Software/Backend/BusinessLogicLayer/AppLogic/Jobs/AddJob/AddJobValidator.cs
@@ -12,7 +12,18 @@ namespace BusinessLogicLayer.AppLogic.Jobs.AddJob
     {
         public AddJobValidator(IJobRepository _jobRepository)
         {
-            
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Naslov je obavezan");
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Opis je obavezan");
+            RuleFor(x => x.Allow_worker_invite)
+                .NotNull().WithMessage("Dozvoli radniku da pozove je obavezan");
+            RuleFor(x => x.Pictures)
+                .NotEmpty().WithMessage("Slike su obavezne")
+                .Must(x => x.Count > 0).WithMessage("Mora postojati barem jedna slika");
+            RuleFor(x => x.Skills)
+                .NotEmpty().WithMessage("Pozicije su obavezne")
+                .Must(x => x.Count > 0).WithMessage("Mora postojati barem jedna pozicija");
         }
     }
 }
