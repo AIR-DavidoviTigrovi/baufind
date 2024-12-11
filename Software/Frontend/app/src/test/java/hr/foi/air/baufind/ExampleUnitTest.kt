7b915d8ab64ad534f4a9f3166234083538cd9428
mstura21-FOI <mustra769@gmail.com>
2024-12-11 01:03:10 +0100

    Promjenjena ikona na navigaciji i test za dodavanje skilova

--- Software/Frontend/app/src/test/java/hr/foi/air/baufind/ExampleUnitTest.kt
+++ Software/Frontend/app/src/test/java/hr/foi/air/baufind/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package hr.foi.air.baufind
 
+import hr.foi.air.baufind.ws.model.Skill
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,7 +13,24 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-
+     fun getAllSkills (){
+        val skill: MutableList<String> = mutableListOf()
+        var skills = listOf(1,2,3)
+        var _skills = listOf(
+            Skill(1, "Vodoinstalater"),
+            Skill(2, "Električar"),
+            Skill(3, "Moler"),
+            Skill(4, "Stolar"),
+            Skill(5, "Keramičar")
+        )
+        if (_skills != null) {
+            for (i in _skills){
+                if(skills.contains(i.id)){
+                    skill +=i.title
+                }
+            }
+        }
+        val result = skill.contains("Vodoinstalater") && skill.contains("Električar") && skill.contains("Moler")
+        Assert.assertTrue(result)
     }
 }
