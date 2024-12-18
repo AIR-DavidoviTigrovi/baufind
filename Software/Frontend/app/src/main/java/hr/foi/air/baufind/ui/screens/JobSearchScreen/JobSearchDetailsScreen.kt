b338a6e997a1aa96bf76e42bacadf47e6b1bd189
vlovric21 <vlovric21@student.foi.hr>
2024-12-18 15:14:45 +0100

    Napravljeno zoomiranje slika

--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/JobSearchScreen/JobSearchDetailsScreen.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/JobSearchScreen/JobSearchDetailsScreen.kt
@@ -3,6 +3,7 @@ package hr.foi.air.baufind.ui.screens.JobSearchScreen
 import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -20,6 +22,10 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
@@ -40,6 +46,8 @@ import hr.foi.air.baufind.ws.network.TokenProvider
 @Composable
 fun JobSearchDetailsScreen(navController: NavController, tokenProvider: TokenProvider, jobSearchViewModel: JobSearchViewModel){
     val selectedJob = jobSearchViewModel.selectedJob.value
+
+    var selectedImageIndex by remember { mutableStateOf<Int?>(null) }
 
     val scrollState = rememberScrollState()
     Column(
@@ -76,17 +84,21 @@ fun JobSearchDetailsScreen(navController: NavController, tokenProvider: TokenPro
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             ){
-                items(selectedJob.pictures) { base64Image ->
+                itemsIndexed(selectedJob.pictures) { index, base64Image ->
                     val imageData = PictureHelper.decodeBase64ToByteArray(base64Image)
                     val bitmap = imageData?.let { BitmapFactory.decodeByteArray(it, 0, it.size) }
 
                     if (bitmap != null) {
+                        val imageSize = if(selectedImageIndex == index) 400.dp else 200.dp
                         Image(
                             bitmap = bitmap.asImageBitmap(),
                             contentDescription = "Slika posla",
                             modifier = Modifier
-                                .size(200.dp)
+                                .size(imageSize)
                                 .padding(8.dp)
+                                .clickable{
+                                    selectedImageIndex = if(selectedImageIndex == index) null else index
+                                }
                         )
                     }
                 }
