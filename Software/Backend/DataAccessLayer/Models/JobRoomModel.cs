79e2aedc3970b0ae807f25390df7ef793b2df106
mstura21-FOI <mustra769@gmail.com>
2024-12-22 17:19:12 +0100

    Napravljen Dataaccess za Job room

---
+++ Software/Backend/DataAccessLayer/Models/JobRoomModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models {
+    public record JobRoom {
+        public int WorkingId { get; set; }
+        public int JobId { get; set; }
+        public string JobTitle { get; set; }
+        public int AllowWorkerInvite { get; set; } 
+        public int SkillId { get; set; }
+        public string SkillTitle { get; set; }
+        public int? WorkerId { get; set; } 
+        public string WorkerName { get; set; }
+        public string WorkingStatus { get; set; }
+    }
+}
