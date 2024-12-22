8ee801542f97530f350d3680a9c9bd237d104578
mstura21-FOI <mustra769@gmail.com>
2024-12-23 00:32:08 +0100

    Treba popravit boju botuna

--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/components/RoleInJobCard.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/components/RoleInJobCard.kt
@@ -4,16 +4,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import hr.foi.air.baufind.R
 import hr.foi.air.baufind.ui.screens.JobRoom.JobRoomScreen
@@ -30,7 +35,7 @@ fun RoleInJobCard(peopleInRoom: Map<String, String>, onItemClick: () -> Unit) {
 
         for (person in helperMap) {
             Column {
-                Text(person.key, modifier = Modifier.padding(6.dp))
+                Text(person.key, modifier = Modifier.padding(6.dp), fontWeight = FontWeight.Bold, fontSize = 20.sp)
                 for (worker in peopleInRoom) {
                     if(worker.value == person.key){
                         if(worker.key != "NULL")
@@ -41,7 +46,10 @@ fun RoleInJobCard(peopleInRoom: Map<String, String>, onItemClick: () -> Unit) {
                 }
                 if(person.value == "NULL"){
                     //Treba implementirati dodavanje korisnika
-                    Button(modifier = Modifier.padding(6.dp), onClick = {}) {
+                    Button(modifier = Modifier.padding(6.dp),colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary, // Primary color from the theme
+                        contentColor = MaterialTheme.colorScheme.onPrimary  // Text color on primary
+                    ), onClick = {}) {
                         Icon(
                             painter = painterResource(id = R.drawable.add_person_icon), // Replace with your drawable
                             contentDescription = "Icon",
