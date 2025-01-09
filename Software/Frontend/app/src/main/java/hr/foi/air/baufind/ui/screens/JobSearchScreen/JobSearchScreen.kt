47bd865c90d07cc1590eec19dfba76d973167c80
Frano Šimić <fsimic21@gmail.com>
2025-01-09 18:06:02 +0100

    Dodan gumb za obavijesti

--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/JobSearchScreen/JobSearchScreen.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/ui/screens/JobSearchScreen/JobSearchScreen.kt
@@ -16,8 +16,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
@@ -33,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,6 +46,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import hr.foi.air.baufind.R
 import hr.foi.air.baufind.service.JobSearchService.JobSearchResponse
 import hr.foi.air.baufind.service.JobSearchService.JobSearchService
 import hr.foi.air.baufind.ui.components.JobListItem
@@ -96,6 +102,17 @@ fun JobSearchScreen(navController: NavController, tokenProvider: TokenProvider, 
         ) {
             Button(onClick = { showCountyBottomSheet = true }) {
                 Text(text = selectedCounty ?: "Odaberite županiju")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(
+                onClick = { }
+            ) {
+                Icon(
+
+                    imageVector = Icons.Outlined.Notifications,
+                    contentDescription = "Notifications",
+                    tint = MaterialTheme.colorScheme.primary
+                )
             }
 
             Spacer(modifier = Modifier.width(8.dp))
