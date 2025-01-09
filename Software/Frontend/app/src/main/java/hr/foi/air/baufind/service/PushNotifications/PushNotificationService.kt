938db8db94b4f0242488170e12fe1ea269a8f76b
dmatijani <dmatijani21@foi.hr>
2025-01-09 15:24:41 +0100

    BAUF-22 Obavijest se prikazuje i kad se korisnik nalazi untar aplikacije
    Još treba napraviti da otvori prikaz posla i kasnije vidjeti kako poslati točan ID posla preko API-ja da aplikacija zna što otvoriti.

--- Software/Frontend/app/src/main/java/hr/foi/air/baufind/service/PushNotifications/PushNotificationService.kt
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/service/PushNotifications/PushNotificationService.kt
@@ -1,7 +1,12 @@
 package hr.foi.air.baufind.service.PushNotifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import hr.foi.air.baufind.R
 
 class PushNotificationService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
@@ -13,6 +18,21 @@ class PushNotificationService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-        // TODO: obrada primljene poruke
+        val title = message.data["title"] ?: "Naslov"
+        val body = message.data["body"] ?: "Sadržaj"
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "default_channel_id"
+
+        val channel = NotificationChannel(channelId, "Default Channel", NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.add_person_icon)
+            .build()
+
+        notificationManager.notify(1, notification)
     }
 }
