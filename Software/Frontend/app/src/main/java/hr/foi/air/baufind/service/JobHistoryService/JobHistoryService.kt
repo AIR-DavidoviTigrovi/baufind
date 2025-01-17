003752ec1fb5afe589c01e02cb859042ab4d47f2
vlovric21 <vlovric21@student.foi.hr>
2025-01-17 13:35:11 +0100

    BAUF-23 Napravljena servisna klasa za povijest poslova i sve klase podataka potrebne za ekrane

---
+++ Software/Frontend/app/src/main/java/hr/foi/air/baufind/service/JobHistoryService/JobHistoryService.kt
@@ -0,0 +1,65 @@
+package hr.foi.air.baufind.service.JobHistoryService
+
+import android.util.Log
+import hr.foi.air.baufind.ws.network.NetworkService
+import hr.foi.air.baufind.ws.network.TokenProvider
+
+class JobHistoryService {
+    suspend fun fetchFullJobHistoryAsync(tokenProvider: TokenProvider): FullJobHistoryResponse {
+        val service = NetworkService.createJobService(tokenProvider)
+        try{
+            val response = service.getFullJobHistory()
+            Log.d("Puna povijest poslova je", response.toString())
+            if(response.error == ""){
+                return FullJobHistoryResponse(
+                    true,
+                    "",
+                    response.jobs
+                )
+            }else{
+                return FullJobHistoryResponse(
+                    false,
+                    response.error,
+                    listOf()
+                )
+            }
+        }catch(e: Exception){
+            e.printStackTrace()
+            Log.d("Puna povijest poslova ERROR", "Error: ${e.message}")
+            return FullJobHistoryResponse(
+                false,
+                "Pogreska pri fetchanju",
+                listOf()
+            )
+        }
+    }
+
+    suspend fun fetchJobHistoryAsync(tokenProvider: TokenProvider): JobHistoryResponse {
+        val service = NetworkService.createJobService(tokenProvider)
+        try{
+            val response = service.getJobHistory()
+            Log.d("Povijest odabranog posla je", response.toString())
+            if(response.error == ""){
+                return JobHistoryResponse(
+                    true,
+                    "",
+                    response.jobHistory
+                )
+            }else{
+                return JobHistoryResponse(
+                    false,
+                    response.error,
+                    null
+                )
+            }
+        }catch(e: Exception){
+            e.printStackTrace()
+            Log.d("Povijest odabranog posla ERROR", "Error: ${e.message}")
+            return JobHistoryResponse(
+                false,
+                "Pogreska pri fetchanju",
+                null
+            )
+        }
+    }
+}
